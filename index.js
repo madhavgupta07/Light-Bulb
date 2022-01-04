fa6bde29fc53a999e59e0144f4ca6f6cf27ad718
Madhav Gupta <72290706+madhavgupta07@users.noreply.github.com>
2022-01-04 13:39:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff');
+const switchs = document.getElementById('switchs')
 const image = document.getElementById('myImage');
+const body = document.getElementById('body');
 const turnOnFunction = () => {
     image.src = 'turnOn.png';
+    switchs.innerHTML = 'Bulb in ON'
+    body.style.backgroundColor = '#ffff99'
 }
 const turnOffFunction = () => {
     image.src = 'turnOff.png';
+    switchs.innerHTML = 'Bulb in OFF'
+    body.style.backgroundColor = '#999999'
 }
 
 
